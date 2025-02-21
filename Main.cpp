65e0c3fd653c66bf44cf924e41e8df6499f5b6c3
DialedB <boljevic.dus4n@gmail.com>
2025-02-21 11:56:19 +0200

    Menu Option 3 works

--- Main.cpp
+++ Main.cpp
@@ -21,21 +21,19 @@ int main() {
             case 2:
                 calculatePerPost(contestants);
             break;
-            case 3: {
+            case 3:
                 double minLikes;
                 cout << "Enter minimum likes per post: ";
                 cin >> minLikes;
                 filterAndSortByLikesPerPost(contestants, minLikes);
                 break;
-            }
             case 4:
                 calculateNewMetric(contestants);
                 saveProcessedData(contestants);
-            break;
-            case 5: {
+                break;
+            case 5:
                 // Implement sorting and saving logic here
                 break;
-            }
             case 6:
                 cout << "Exiting..." << endl;
             break;
